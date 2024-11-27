3ff497bcd3d620d6863818cf4ba60d1703418bb9
Vlad Korenev <korenevlad17@gmail.com>
2024-11-27 21:59:21 +0300

    27_11_24_3

--- src/Edu.StockApi.Web/Configuration/Swagger/HeaderOperationFilter.cs
+++ src/Edu.StockApi.Web/Configuration/Swagger/HeaderOperationFilter.cs
@@ -4,19 +4,19 @@ using Swashbuckle.AspNetCore.SwaggerGen;
 namespace Edu.StockApi.Web;
 
 //  Добавляет новые параметры к запросу 
-public abstract class HeaderOperationFilter: IOperationFilter
+public class HeaderOperationFilter: IOperationFilter
 {
     public void Apply(OpenApiOperation operation, OperationFilterContext context)
     {
         //  проверили, есть ли список
         operation.Parameters ??= new List<OpenApiParameter>();
         //   добавили header
-        operation.Parameters.Add(new OpenApiParameter(new OpenApiParameter
+        operation.Parameters.Add(new OpenApiParameter
         {
             In = ParameterLocation.Header,
             Name = "our-header",
-            Required = false,   
-            Schema = new OpenApiSchema{Type = "string"}
-        }));
+            Required = false,
+            Schema = new OpenApiSchema { Type = "string" }
+        });
     }
 }
